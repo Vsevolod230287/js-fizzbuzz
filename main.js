aeda0761e11e2f957a2e885bd0ac03e73d5d44c7
Vsevolod <cerneavschivsevolod@gmail.com>
2021-03-05 16:02:28 +0100

    Esercizio finito senza (stili).

--- main.js
+++ main.js
@@ -2,18 +2,14 @@
 // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-for ( i = 1; i <= 100; i++) {
-  // var print = document.getElementById('risultato').innerHTML += i;
-  if(!(i%3)) {
-    console.log("Fizz");
-    // var print = document.getElementById('risultato').innerHTML += "Fizz";
-  } else if (!(i%5)) {
-    console.log("Buzz");
-    // var print = document.getElementById('risultato').innerHTML += "Buzz";
-  }else if ((!(i%3)) && (!(i%5))) {
-    console.log("FizzBuzz");
-    // var print = document.getElementById('risultato').innerHTML += "FizzBuzz";
-  }else {
-    console.log(i);
+for (i = 1; i <= 100; i++) {
+  if (!(i % 3) && (!(i % 5))) {
+    print = document.getElementById('risultato').innerHTML += "<div>" + "FizzBuzz"  + "</div>";
+  } else if (!(i % 5)) {
+    print = document.getElementById('risultato').innerHTML += "<div>" + "Buzz"  + "</div>";
+  } else if (!(i % 3)) {
+    print = document.getElementById('risultato').innerHTML += "<div>" + "Fizz"  + "</div>";
+  } else {
+    print = document.getElementById('risultato').innerHTML += "<div>" + i + "</div>";
   }
 }
